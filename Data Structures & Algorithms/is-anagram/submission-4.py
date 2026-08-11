29feeb7c6d66571d88b4f87e11e4bfac39d6b128
Neljo1 <neljojaison@gmail.com>
2026-08-11 21:45:07 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss=sorted(s)
+        st=sorted(t)
+        if ss==st:
+            return True
+        else :
+            return False
